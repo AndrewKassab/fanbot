cf38406d577864b6c4a168a0e42344d6ed070406
akassab <andrewkassab@live.com>
2021-08-27 17:23:52 -0700

    get artist id working

--- spotify.py
+++ spotify.py
@@ -1,22 +1,25 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
 class InvalidArtistException(Exception):
 
     def __init__(self):
         super().__init__("Invalid Artist")
 
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+client_id = "51303df7e9c245e488c1120dfad0b744"
+client_secret = "01e46229d55b4102bcec68debc31bda7"
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
 
 
-# TODO: Retrieve artist id from searching the artist name on spotify api
 def get_artist_id(artist_name):
     results = sp.search(q='artist:' + artist_name, type='artist')
     items = results['artists']['items']
     if len(items) > 0:
         artist = items[0]
-        # TODO: return artist id
+        return artist['id']
     else:
         raise InvalidArtistException
 
--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -5,7 +5,7 @@ from spotify import InvalidArtistException, get_artist_id
 class TestSpotify(unittest.TestCase):
 
     def test_get_artist_id_invalid(self):
-        self.assertRaises(InvalidArtistException, get_artist_id("some invalid artist name"))
+        self.assertRaises(InvalidArtistException, get_artist_id, "invalid artist name")
 
     def test_get_artist_id_valid(self):
         artist_id = get_artist_id("Porter Robinson")
