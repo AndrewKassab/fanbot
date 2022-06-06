9232f9835644d91266d0ed92e2bed103e50e699b
akassab <andrewkassab@live.com>
2022-06-06 14:18:33 -0700

    fixed test

--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -4,14 +4,15 @@ import unittest
 
 class TestSpotify(unittest.TestCase):
 
-    valid_artist_name = 'Porter Robinson'
+    valid_artist = 'https://open.spotify.com/artist/3dz0NnIZhtKKeXZxLOxCam?si=522cf0f2c0bd4f6f'
     valid_artist_true_id = '3dz0NnIZhtKKeXZxLOxCam'
 
     def test_get_artist_id_invalid(self):
         self.assertRaises(InvalidArtistException, get_artist_by_id, "invalid artist name")
 
     def test_get_artist_id_valid(self):
-        artist = get_artist_by_id(self.valid_artist_name)
+        artist_id = extract_artist_id(self.valid_artist)
+        artist = get_artist_by_id(artist_id)
         self.assertEqual(self.valid_artist_true_id, artist.id)
 
     def test_get_artist_newest_release(self):
