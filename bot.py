332208ee9c2f841dbb3b100ba6655f0a88a5fab4
akassab <andrewkassab@live.com>
2022-07-21 17:47:09 -0700

    removed parameter name

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ async def on_raw_reaction_add(payload):
     reaction = payload.emoji
     role_string = message.content.split()[0]
     role_id = int(role_string[3:len(role_string) - 1])
-    role = guild.get_role(role_id=role_id)
+    role = guild.get_role(role_id)
     if role is None:
         return
     if reaction.name == FOLLOW_ROLE_EMOJI:
