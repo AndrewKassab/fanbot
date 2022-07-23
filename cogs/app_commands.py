de45969794ff6238c6470477c77e2f7273659f85
akassab <andrewkassab@live.com>
2022-07-22 17:19:41 -0700

    shortened follow method

--- cogs/app_commands.py
+++ cogs/app_commands.py
@@ -3,6 +3,7 @@ from config.commands import *
 from utils.spotify import *
 from utils.database import Guild
 from discord.ext import commands
+from mysql.connector.errors import IntegrityError
 import logging
 from discord import app_commands
 import discord
@@ -47,29 +48,36 @@ class AppCommandsCog(commands.Cog):
                 content="Artist not found, please make sure you are providing a valid spotify artist url")
             return
 
-        artist_in_db = self.bot.db.get_artist_for_guild(artist.id, interaction.guild_id)
-        if artist_in_db is not None:
-            role = interaction.guild.get_role(int(artist_in_db.role_id))
-            if role is None:
-                self.bot.db.remove_artist(artist_in_db)
-            else:
-                await interaction.edit_original_message(content='This server is already following %s! We\'ve assigned '
-                                                                'you the corresponding role.' % artist_in_db.name)
-                if isinstance(interaction.user, discord.Member):
-                    await interaction.user.add_roles(role)
-                return
-
-        role = await interaction.guild.create_role(name=(artist.name.replace(" ", "") + 'Fan'))
+        artist.guild_id = interaction.guild_id
+        role = await self.get_role_for_artist(artist)
         artist.role_id = role.id
-        artist.guild_id = interaction.guild.id
+
         try:
             self.bot.db.add_artist(artist)
-        except Exception as e:  # TODO: Make specific
+            await self.send_successful_follow_message(artist, interaction)
+        except IntegrityError:
+            await interaction.edit_original_message(content='This server is already following %s! We\'ve assigned '
+                                                            'you the corresponding role.' % artist.name)
+        except Exception as e:
             await interaction.edit_original_message(content="Failed to follow artist.")
             await role.delete()
             logging.exception('Failure to follow artist and add to db: ', str(e))
             return
 
+        await interaction.user.add_roles(role)
+
+    async def get_role_for_artist(self, artist):
+        artist_db = self.bot.get_artist_for_guild(artist.id, artist.guild_id)
+        if artist_db is not None:
+            role = self.bot.get_guild(artist_db.id).get_role(artist_db .role_id)
+            if role is None:
+                self.bot.db.remove_artist()
+                role = await self.bot.get_guild(artist_db.guild_id).create_role(name=(artist_db.name.replace(" ", "") + 'Fan'))
+        else:
+            role = await self.bot.get_guild(artist.guild_id).create_role(name=(artist.name.replace(" ", "") + 'Fan'))
+        return role
+
+    async def send_successful_follow_message(self, artist, interaction):
         logging.info(f"Guild {interaction.guild_id} has followed a new artist: {artist.name} {artist.id}")
         await interaction.edit_original_message(
             content="<@&%s> %s has been followed!\n:white_check_mark:: Assign Role. :x:: Remove Role."
@@ -77,7 +85,6 @@ class AppCommandsCog(commands.Cog):
         message = await interaction.original_message()
         await message.add_reaction(FOLLOW_ROLE_EMOJI)
         await message.add_reaction(UNFOLLOW_ROLE_EMOJI)
-        await interaction.user.add_roles(role)
 
     @app_commands.command(
         name=LIST_COMMAND,
