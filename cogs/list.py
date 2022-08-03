50039d1012d713bb6295418721bcaa2acf1105ff
akassab <andrewkassab@live.com>
2022-08-02 18:59:45 -0700

    made list command ephemeral

--- cogs/list.py
+++ cogs/list.py
@@ -14,7 +14,7 @@ class List(commands.Cog):
         description="list followed artists",
     )
     async def list_follows(self, interaction: discord.Interaction):
-        await interaction.response.send_message("Attempting to list all followed artists...")
+        await interaction.response.send_message("Attempting to list all followed artists...", ephemeral=True)
         artists = self.bot.db.get_all_artists_for_guild(guild_id=interaction.guild_id)
         await interaction.edit_original_message(
             content="Following Artists: %s" % list(artist.name for artist in artists.values()))
