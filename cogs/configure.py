1f0a4168866b282b8ab9098acd8c5ddc28fe67e0
akassab <andrewkassab@live.com>
2022-08-02 18:56:48 -0700

    added error handling to missing permission for set update channel

--- cogs/configure.py
+++ cogs/configure.py
@@ -19,8 +19,13 @@ class Configure(commands.Cog):
         await interaction.response.send_message("Attempting to configure current channel for updates...")
         if not self.bot.db.is_guild_in_db(interaction.guild_id):
             self.bot.db.add_guild(Guild(interaction.guild_id, interaction.channel_id))
-            await interaction.edit_original_message(content="Current channel successfully configured for updates. "
-                                                            f"You may begin following artists using `/{FOLLOW_COMMAND}`.")
+            await interaction.edit_original_message(content="Channel successfully configured for updates. You "
+                                                            f"may begin following artists using `/{FOLLOW_COMMAND}`.")
         else:
             self.bot.db.update_guild_channel_id(interaction.guild_id, interaction.channel_id)
             await interaction.edit_original_message(content="Current channel successfully configured for updates.")
+
+    @set_update_channel.error
+    async def set_update_channel_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingPermissions):
+            interaction.response.send_message(content="Only Administrators can issue this command", ephemeral=True)
